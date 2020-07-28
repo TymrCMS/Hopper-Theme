340b4aa57fd9af0604e1d7efd1ebfe5e8ccaa73b
Sam McDonald <s.mcdonald@outlook.com.au>
2020-07-29 08:42:16 +1000

    updated theme

--- public/components/footer/default.blade.php
+++ public/components/footer/default.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
 	<div class="pull-right hidden-xs">
 		Version <small>0.0.1</small>
-		{ SiteMap() !!}
+		{ SiteMap() }
 	</div>
-	<small>Copyright &copy; 2018 <a href="http://tymr.app">Tymr Corp</a>. All rights reserved.</small>
+	<small>Copyright &copy; {{now()->year }} <a href="http://tymr.app">Tymr Corp</a>. All rights reserved.</small>
 </footer>
